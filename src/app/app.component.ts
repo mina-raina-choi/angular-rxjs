9499d67fec9ad081a09502f66c8a61eec13e6d64
minami-choi <cmina2846@gmail.com>
2018-07-20 18:15:34 +0900

    Angular observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs'
-import { map, take } from 'rxjs/operators';
+import { map, take, filter } from 'rxjs/operators';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
@@ -8,13 +12,54 @@ import { map, take } from 'rxjs/operators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  form: FormGroup;
+  comment = new FormControl("", Validators.required);
+  name = new FormControl("", Validators.required);
+  email = new FormControl("", [
+    Validators.required,
+    Validators.pattern("[^ @]*@[^ @]*")
+  ]);
 
-  constructor() {
-    this.getStream.subscribe(value => console.log("Subscriber: " + value));
+
+  constructor(fb: FormBuilder) {
+    // this.getStream.subscribe(value => console.log("Subscriber: " + value));
+    this.form = fb.group({
+      "comment": this.comment,
+      "name": this.name,
+      "email": this.email
+    });
+
+    // using observables
+    this.form.valueChanges
+      .filter(data => this.form.valid)
+      .map(data => {
+        data.comment = data.comment.replace(/<(?:.|\n)*?>/gm, '');
+        return data
+      })  
+      .map(data => {
+        data.lastUpdateTS = new Date();
+        return data
+      })
+      .subscribe( data => console.log(JSON.stringify(data)))
+
+    //not using observables
+    // this.form.valueChanges
+    //   .subscribe(data => {
+    //     if (this.form.valid) {
+    //       data.comment = data.comment.replace(/<(?:.|\n)*?>/gm, '');
+    //       data.lastUpdateTS = new Date();
+    //       console.log(JSON.stringify(data))
+    //     }
+    //   })
+
   }
-  
-  getStream = Observable.interval(1000)
-  .take(3)
-  .map(() => Date.now())
+
+  onSubmit() {
+    console.log("Form submitted!")
+  }
+
+  // getStream = Observable.interval(1000)
+  // .take(3)
+  // .map(() => Date.now())
 
 }
